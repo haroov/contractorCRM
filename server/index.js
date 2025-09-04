a7aeccd8b41e66ee97eee0b3994a9f077deafb3d
Liav Geffen <liav@geffen.org.il>
2025-09-04 18:05:13 +0300

    Fix CORS to allow X-Session-ID header

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'X-Session-ID'],
   exposedHeaders: ['Set-Cookie']
 }));
 app.use(express.json());
